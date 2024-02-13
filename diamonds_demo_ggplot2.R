3b302bb715951c2057499f5e988dd36bf3bd5fda
darrengunadharma1 <darrengu@uw.edu>
2024-02-13 08:36:07 -0800

    added demo file

---
+++ diamonds_demo_ggplot2.R
@@ -0,0 +1,24 @@
+library(ggplot2)
+data(diamonds)
+summary(diamonds)
+
+#Let's start by adding one variable at a time - starting with the carat variable
+
+# Now let's add price
+
+# Let's add some geoms i.e. geometric objects (geom_point + geom_smooth)
+
+# Adding aesthetics and more variables
+
+#Let's try specifying aesthetics for each geom
+
+#Labels and themes
+
+# Lastly, let's take a look at facets and faceting. By faceting we are generating
+# small multiple graphs where each graph is showing a different subset of the data
+
+# What if we wanted to visually examine how price and carat interacts with not
+# only cut but also clarity - this is now 4 variables but we only have a 2d grid 
+# to work with. 
+
+
